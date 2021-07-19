b0efb18a8d5b3fbbc146a039e56af6964880bc60
chandg <chandg@tcd.ie>
2021-07-19 22:49:34 +0100

    mior fixes

--- backend/to-do-list/src/test/java/com/gc/todolist/controller/TaskToDoCotrollerTest.java
+++ backend/to-do-list/src/test/java/com/gc/todolist/controller/TaskToDoCotrollerTest.java
@@ -1,5 +1,42 @@
 package com.gc.todolist.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+
+import com.gc.todolist.model.TaskToDo;
+import com.sun.security.auth.UserPrincipal;
+
 public class TaskToDoCotrollerTest {
 
+	
+	TaskToDoController taskToDoController;
+
+	
+	@Before(value = "")
+	public void initialSetUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		UserPrincipal user = new UserPrincipal("test");
+		TaskToDoController todoController = new TaskToDoController();
+	}
+	
+	
+	//TODO: Implementation of unit test is pending
+	
+	
+	@Test
+	public void getTodoListByUserId() {
+		TaskToDo taskToDo = new TaskToDo();
+		List<TaskToDo> todoData = new ArrayList<>();
+				todoData.add(taskToDo);
+		//when(TaskToDoController.getTaskToDoDetils()).thenReturn(todoData);
+		//List<TaskToDo> toDos = TaskToDoController.getTaskToDoDetils();
+		//assertEquals(1, todos.size());
+		
+			
+				
+	}
 }
